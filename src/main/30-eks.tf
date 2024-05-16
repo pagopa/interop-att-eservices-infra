538580bbc02f324ac8318fa40410cdeeb900b22d
Francesco Carlucci <francesco.carlucci@accenture.com>
2024-05-16 10:28:15 +0200

    feat(ADA-33): k8s configuration

--- src/main/30-eks.tf
+++ src/main/30-eks.tf
@@ -40,11 +40,12 @@ locals {
     "NAMESPACE"                   = var.namespace,
     "SERVICEACCOUNT"              = kubernetes_service_account.service_account.metadata.0.name,
     "RESIDENCEVERIFICATIONIMAGE"  = format("%s.dkr.ecr.%s.amazonaws.com/%s:%s", data.aws_caller_identity.current.account_id, var.aws_region, "interop-att-eservice-residence-verification", replace(var.reference_branch, "/", "-")),
-    "DATABASE_URL"                = format("postgres://%s:%s@%s:%s/%s", module.aurora_postgresql_v2.cluster_master_username, random_password.master.result, module.aurora_postgresql_v2.cluster_endpoint, module.aurora_postgresql_v2.cluster_port, module.aurora_postgresql_v2.cluster_database_name)
     "FISCALCODEVERIFICATIONIMAGE" = format("%s.dkr.ecr.%s.amazonaws.com/%s:%s", data.aws_caller_identity.current.account_id, var.aws_region, "interop-att-eservice-fiscalcode-verification", replace(var.reference_branch, "/", "-")),
-    "LIQUIBASE_DATABASE_URL"      = format("postgres://%s:%s/%s", module.aurora_postgresql_v2.cluster_endpoint, module.aurora_postgresql_v2.cluster_port, module.aurora_postgresql_v2.cluster_database_name),
-    "LIQUIBASE_DATABASE_USERNAME" = format("%s", module.aurora_postgresql_v2.cluster_master_username),
-    "LIQUIBASE_DATABASE_PASSWORD" = format("%s", random_password.master.result),
+    "DATABASE_URL"                = format("%s:%s", module.aurora_postgresql_v2.cluster_endpoint, module.aurora_postgresql_v2.cluster_port)
+    "DATABASE_USERNAME"           = format("%s", module.aurora_postgresql_v2.cluster_master_username),
+    "DATABASE_PASSWORD"           = format("%s", random_password.master.result),
+    "DATABASE_NAME"               = format("%s", module.aurora_postgresql_v2.cluster_database_name),
+    "DATABASE_SCHEMA"             = format("%s", module.aurora_postgresql_v2.cluster_master_username),
   }
 }
 
