fbf70154b72901cb8f02de7a8fa7a5518643a645
francarl77 <97600561+francarl77@users.noreply.github.com>
2024-06-03 12:25:45 +0200

    feat(ADA-43): k8s configuration for digital-address-verification eservice

--- src/main/98-variables.tf
+++ src/main/98-variables.tf
@@ -98,7 +98,7 @@ variable "fiveXXs_thresholds" {
 variable "packages" {
   type        = list(any)
   description = "Packages of att-services repository"
-  default     = ["residence-verification", "fiscalcode-verification", "piva-verification", "trial-service-api"]
+  default     = ["residence-verification", "fiscalcode-verification", "piva-verification", "trial-service-api", "digital-address-verification"]
 }
 
 variable "reference_branch" {
